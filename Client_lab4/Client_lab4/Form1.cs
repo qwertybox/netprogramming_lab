e0854f0fefe5eaaa34272ec9e0d84721a4f147fc
Bohdan <ovsak.bohdan@gmail.com>
2021-02-10 14:03:47 +0200

    added

---
+++ Client_lab4/Client_lab4/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Xml.Linq;
+
+namespace Client_lab4
+{
+    public partial class Form1 : Form
+    {
+        protected string Answer;
+        string A;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string abc = textBox1.Text;
+            A = Speaking(abc);
+            XDocument xrequest = new XDocument();
+            XElement Search_pattern = new XElement("Search_pattern");
+            XAttribute patternAttr = new XAttribute("Pattern", abc);
+            Search_pattern.Add(patternAttr);
+            xrequest.Add(Search_pattern);
+            xrequest.Save("Request-1.XML");
+            label2.Text = A;
+            WriteLog(A);
+        }
+
+        public string Speaking(string theMessage)
+        {
+            string F = File.ReadAllText(@"C:\Mine\Labs\Lab-4\Client_lab4\Client_lab4\bin\Debug\Request-1.xml");
+            byte[] M = Encoding.UTF8.GetBytes(F);
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 49000);
+            byte[] bytes = new byte[1000000];
+            using (Socket S = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
+            {
+                S.Connect(ipEndPoint);
+                S.Send(M);
+                int bytesRec = S.Receive(bytes);
+                Answer = Encoding.GetEncoding(1251).GetString(bytes, 0, bytesRec);
+                S.Shutdown(SocketShutdown.Both);              
+                return Answer;
+            }
+        }
+
+        private static void WriteLog(string z)
+        {
+            using (StreamWriter F = new StreamWriter(@"C:\Mine\Labs\Lab-4\logs\log.txt", true))
+            {
+                F.WriteLine(DateTime.Now + z);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Server_lab4/Server_lab4/Service1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Xml.Linq;
+using System.Management;
+using Microsoft.Win32;
+using System.Text.RegularExpressions;
+
+namespace Server_lab4
+{
+    public partial class Service1 : ServiceBase
+    {
+        RegistryHive h = RegistryHive.LocalMachine;
+        int port = 49000;
+        Thread Thread1;
+        bool mustStop;
+        public static string answer;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Thread1 = new Thread(WorkerThread);
+            Thread1.Start();
+        }
+
+        protected override void OnStop()
+        {
+            if ((Thread1 != null) && (Thread1.IsAlive))
+            {
+                mustStop = true;
+            }
+        }
+
+        void WorkerThread()
+        {
+            while (!mustStop)
+            {
+                IPAddress IP = IPAddress.Parse("192.168.1.106");
+                IPEndPoint ipEndPoint = new IPEndPoint(IP, port);
+                Socket S = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                try
+                {
+                    S.Bind(ipEndPoint);
+                    while (true)
+                    {
+                        IPEndPoint L = new IPEndPoint(IP, 0);
+                        EndPoint R = (EndPoint)(L);
+                        byte[] D = new byte[1000000];
+                        int Receive = S.ReceiveFrom(D, ref R);
+                        string Request = Unpack(Encoding.GetEncoding(1251).GetString(D, 0, Receive));
+                        XDocument request_1 = new XDocument();
+                        request_1 = MakeXML(Request);
+                        if (!File.Exists(@"C:\Mine\Labs\laba_4\logs_server\Request-1.xml"))
+                        {
+                            request_1.Save(@"C:\Mine\Labs\laba_4\logs_server\Request-1.xml");
+                        }
+                        else
+                        {
+                            request_1.Save(@"C:\Mine\Labs\laba_4\logs_server\Request-2.xml");
+                        }
+                        XDocument response;
+                        response = Make_List(GetSurnamesByFilter(Request));
+                        answer = response.ToString();
+                        if (!File.Exists(@"C:\Mine\Labs\laba_4\logs_server\Response-1.xml"))
+                        {
+                            response.Save(@"C:\Mine\Labs\laba_4\logs_server\Response-1.xml");
+                        }
+                        else
+                        {
+                            response.Save(@"C:\Mine\Labs\laba_4\logs_server\Response-2.xml");
+                        }
+
+                        if (Request.Length > 4)
+                        {
+                            byte[] M = Encoding.GetEncoding(1251).GetBytes(answer);
+                            S.SendTo(M, R);
+                        }
+                        else
+                        {
+                            byte[] M = Encoding.GetEncoding(1251).GetBytes(answer);
+                            S.SendTo(M, R);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+
+        static string[] GetSurnamesByFilter(string d)
+        {
+            var keyvalues = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64).
+                           OpenSubKey(@"Software\udpparameters").GetValueNames();
+            var keyvalues1 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64).
+                            OpenSubKey(@"Software\udpparameters").GetValueNames();
+
+            for (int i = 0; i < keyvalues.Length; i++)
+            {
+                keyvalues[i] = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64).
+                            OpenSubKey(@"Software\udpparameters").GetValue(keyvalues[i]).
+                            ToString().Substring(0, RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64).
+                            OpenSubKey(@"Software\udpparameters").GetValue(keyvalues[i]).
+                            ToString().IndexOf(' '));
+                keyvalues1[i] = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64).
+                           OpenSubKey(@"Software\udpparameters").GetValue(keyvalues1[i]).ToString();
+            }
+            if (d.StartsWith("*"))
+            {
+                if (d == "*")
+                {
+                    return keyvalues;
+                }
+                else
+                {
+                    d = d.Substring(1);
+                    var result = new List<string>();
+                    int k = 0;
+                    for (int i = 0; i < keyvalues.Length; ++i)
+                    {
+                        if (keyvalues[i].EndsWith(d)) { result.Add((keyvalues[i])); ++k; }
+                    }
+                    return result.ToArray();
+
+                }
+            }
+            else if (d.EndsWith("*"))
+            {
+                string g = "";
+                foreach (char ch in d)
+                {
+                    string str = ch.ToString();
+                    if (Regex.IsMatch(str, @"[а-я]$", RegexOptions.IgnoreCase))
+                    {
+                        g += str;
+                    }
+                }
+                string[] result = new string[1000];
+                int k = 0;
+                for (int i = 0; i < keyvalues.Length; ++i)
+                {
+                    if (keyvalues[i].StartsWith(g)) { result[k] = (keyvalues[i]); k++; }
+                }
+                return result;
+            }
+            else
+            {
+                string[] result = new string[1000];
+                int k = 0;
+                for (int i = 0; i < keyvalues.Length; ++i)
+                {
+                    if (keyvalues[i] == d) result[k++] = (keyvalues1[i]);
+                }
+                return result;
+            }
+        }
+        XDocument Make_List(string[] a)
+        {
+            var doc = new XDocument();
+            var el = new XElement("list");
+            for (int i = 0; i < a.Length; i++)
+            {
+                el.Add(new XElement("students", a[i]));
+            }
+            doc.Add(el);
+            return doc;
+        }
+        string Unpack(string g)
+        {
+            var t = XDocument.Parse(g);
+            return t.Element("filt").Value;
+        }
+        XDocument MakeXML(string name)
+        {
+            XDocument doc = new XDocument();
+
+            XElement f = new XElement("students", name);
+            doc.Add(f);
+            return doc;
+        }
+    }
+}
